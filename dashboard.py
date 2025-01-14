b67b20f62039d38d49a0b0bd9b2d7355cc038184
Firman Farel Richardo <farelpasaribu04@gmail.com>
2025-01-14 15:17:53 +0700

    update dataframe

--- dashboard.py
+++ dashboard.py
@@ -5,3 +5,16 @@ import streamlit as st
 from babel.numbers import format_currency
 
 sns.set(style='dark')
+
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_date').agg({
+        "order_id": "nunique",
+        "total_price": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "total_price": "revenue"
+    }, inplace=True)
+    
+    return daily_orders_df
