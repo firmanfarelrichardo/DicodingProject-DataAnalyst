d086275f8a09513628035fc4a88f76bff8fe9ec0
Firman Farel Richardo <farelpasaribu04@gmail.com>
2025-01-21 12:24:29 +0700

    update notebook, all data dan dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,19 +6,3 @@ from babel.numbers import format_currency
 
 sns.set(style='dark')
 
-def create_daily_orders_df(df):
-    daily_orders_df = df.resample(rule='D', on='order_date').agg({
-        "order_id": "nunique",
-        "total_price": "sum"
-    })
-    daily_orders_df = daily_orders_df.reset_index()
-    daily_orders_df.rename(columns={
-        "order_id": "order_count",
-        "total_price": "revenue"
-    }, inplace=True)
-    
-    return daily_orders_df
-
-def create_sum_order_items_df(df):
-    sum_order_items_df = df.groupby("product_name").quantity_x.sum().sort_values(ascending=False).reset_index()
-    return sum_order_items_df
